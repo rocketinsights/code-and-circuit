7a86be9f2d1c5f81d65f9de5c77fda892b5bc35f
Jesse Streb <jesse.streb@gmail.com>
2019-12-04 09:31:17 -0500

    Checking in a working skill.

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
+const _ = require('lodash')
 const alexa = require('alexa-app');
 const app = new alexa.app();
 
+const movies = [
+    {
+        name: 'anchorman',
+        url: 'https://rocket-code-and-circuit.s3.amazonaws.com/anchorman.mp3'
+    },
+    {
+        name: 'blades of glory',
+        url: 'https://rocket-code-and-circuit.s3.amazonaws.com/oldschool.mp3'
+    },
+    {
+        name: 'old school',
+        url: 'https://rocket-code-and-circuit.s3.amazonaws.com/oldschool.mp3'
+    }
+]
+
 app.launch(function(req,res) {
-  res
-    .shouldEndSession(false)
-    .session('answer', 'anchorman')
-    .say('Hello blank, to your movie game. Here is your first movie. <audio src="https://rocket-code-and-circuit.s3.amazonaws.com/anchorman.mp3" />')
-    .send()
+    const clips = _.shuffle(movies)
+    const clip = clips.pop()
+    const session = req.getSession()
+    session.set('name', clip.name)
+    session.set('clips', JSON.stringify(clips))
+           
+    res
+        .shouldEndSession(false)
+        .say(`Hello blank, to your movie game. Here is your first movie. <audio src="${clip.url}" /> What is the movie?`)
+        .send()
 });
 
 app.intent('GuessIntent', function(req, res) {
-  const guess = req.slot('movie')
-  const answer = req.session('answer')
-  console.log(`The user guessed ${guess} and the answer was ${answer}`)
-  res
-    .shouldEndSession(false)
-    .say('Yahoo, things are working! <amazon:effect name="whispered">Sort of</amazon:effect>')
-    .send()
+    const guess = req.slot('movie')
+    const session = req.getSession()
+    const answer = session.get('name')
+    const clips = JSON.parse(session.get('clips'))
+    const clip = clips.pop()
+    let shouldEndSession
+
+    console.log(`The user guessed ${guess} and the answer was ${answer}`)
+
+    let msg = (guess.toLowerCase() === answer.toLowerCase()) ? 'Hot damn, you are correct!' : 'Nope.'
+    if (clip) {
+        msg = `${msg} Your next movie is. <audio src="${clip.url}" /> What is the movie?`
+        session.set('name', clip.name)
+        session.set('clips', JSON.stringify(clips))
+        shouldEndSession = false
+    } else {
+        msg = `${msg} Thanks for playing!`
+        shouldEndSession = true
+    }
+
+    res
+        .shouldEndSession(shouldEndSession)
+        .say(msg)
+        .send()
 })
 
 app.sessionEnded(function(req, res) {
-  res.say('time is up. good bye.').send();
+    res.say('time is up. good bye.').send();
 })
 
 app.error = function(exception, req, res) {
-  console.log(exception)
-  res.say('Oh no, something went wrong.').send()
+    console.log(exception)
+    res.say('Oh no, something went wrong.').send()
 }
 
 exports.handler = app.lambda()
