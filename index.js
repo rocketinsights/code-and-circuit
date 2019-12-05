d43d3dc8ae943994159ad6587f610998df736dc9
Brian Manning <bri@brimanning.com>
2019-12-05 18:56:40 -0500

    Add question after answer

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const alexa = require('alexa-app');
+const _ = require('lodash')
 const app = new alexa.app();
 
+var answer;
+var movies = ["https://rocket-code-and-circuit.s3.amazonaws.com/blade_of_glory.mp3", "https://rocket-code-and-circuit.s3.amazonaws.com/oldschool.mp3", "https://rocket-code-and-circuit.s3.amazonaws.com/anchorman.mp3"]
+var answers = ["blades of glory", "old school", "anchorman"]
+var playMovieQuote;
+
+function setNewMovie(){
+    var random = Math.floor((Math.random())*movies.length)
+    console.log(random);
+    playMovieQuote = movies[random];
+    answer = answers[random];
+}
+
+function sayNewMovie(){
+    setNewMovie();
+    return `Next Quote: <audio src="${playMovieQuote}" />`
+}
+
 app.launch(function(req,res) {
+    setNewMovie();
   res
     .shouldEndSession(false)
-    .session('answer', 'anchorman')
-    .say('Hello blank, to your movie game. Here is your first movie. <audio src="https://rocket-code-and-circuit.s3.amazonaws.com/anchorman.mp3" />')
+    .say(`Welcome to the movie trivia game! Listen to a quote, and then guess what movie it came from. <audio src="${playMovieQuote}" />`)
     .send()
 });
-
+//asd
 app.intent('GuessIntent', function(req, res) {
   const guess = req.slot('movie')
-  const answer = req.session('answer')
   console.log(`The user guessed ${guess} and the answer was ${answer}`)
+  var correct;
+  if(guess == answer){
+      correct = "Correct";
+  }else{
+      correct = "Incorrect";
+  }
+  var oldMovie = _.cloneDeep(answer);
+  var newMovie = sayNewMovie();
   res
     .shouldEndSession(false)
-    .say('Yahoo, things are working! <amazon:effect name="whispered">Sort of</amazon:effect>')
+    .say(correct + ", The answer was: " + oldMovie + ".")
+    .say(newMovie)
     .send()
 })
 
